36c5ab511b58ee9338e09a2e3605678521706da1
Akmal <akmal@tinkertanker.com>
2024-06-03 13:45:06 +0800

    add platformio build

---
+++ DroidKitESP/src/config.h
@@ -0,0 +1,20 @@
+#ifndef Config_h
+#define Config_h
+
+#define MOTOR_1_PIN_1    27
+#define MOTOR_1_PIN_2    26
+#define SERVO_PIN        25
+#define SERVO_MID_POS    1300
+#define SERVO_SIDE_RANGE 60
+
+#define SPEAKER_PIN      16
+
+// Neo pixel code
+#define PIN 12  // LED D pin
+#define LEDno 10  // LED D pin
+
+// Replace with your network credentials
+extern const char* ssid = "ESP32-Droid";
+extern const char* password = "DroidKitESP";
+
+#endif
